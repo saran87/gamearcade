557ce37ca1500035e8396a3d5a083de2dc125d48
SaravanaKumar <saran87@gmail.com>
2012-11-01 03:40:01 -0400

    Chat online status image and minimize functionality

--- view/js/chat.js
+++ view/js/chat.js
@@ -4,15 +4,18 @@
 *
 */
 (function(window,undefined){
+	//chat object
 	var chat = function(){
 	
 	};
+	//function to update chat panel, which displays current logged in users
 	chat.updateChatPanel = function (){
 		
 		var site = new Site();
 		site.getUserList(chat.updateUsers);
 		
 	};
+	//call back function to handle to update user list in the chat panel
 	chat.updateUsers = function (data){
 
 			var list = $("#chat_content").find("ul");
@@ -23,14 +26,14 @@
 					list.append(createListItem(this));
 				
 				});
+				//update chat panel every 20 secs
+				//setTimeout(chat.updateChatPanel,2000);
 			}else if(data.error){
 			
 				if(data.error.isLoginRequired){
 					showLoginModal();
 				}			
 			}
-		//update chat panel every 20 secs
-		setTimeout(chat.updateChatPanel,2000);
 	};
 	chat.initializeChat = function(response){
 		
@@ -46,18 +49,34 @@
 			var data = {"message":chatWindow.find('.message').val(),"chatid":response.data.chatId};
 			var site = new Site();
 			site.sendMessage(data,chat.updateMessage);
+		}else if(data.error){
 			
+				if(data.error.isLoginRequired){
+					showLoginModal();
+				}			
 		}
 	};
 	chat.updateMessage = function(response){
-		if( !response.error){
+		if( !response.error && response.data){
 			//get the chat window from the dom
-			var chatWindow = document.getElementById(response.data.chatId);
-				chatWindow = $(chatWindow);//get jquery object
-				chatWindow.text(response.data.message);
-			console.log(response);
+			var messagePanel = $("#chat_message_template").clone().removeClass('hide');
+				messagePanel.find('.name').text(response.data.name);
+				messagePanel.find('.message').text(response.data.message);
+				messagePanel.find('.timeStamp').text(response.data.timestamp);
+				
+				messagePanel.appendTo($(document.getElementById(response.data.chatId)));
+				messagePanel.focus();
+				messagePanel.parent().scrollTop(messagePanel.parent().scrollHeight - messagePanel.parent().clientHeight);
+				console.log(response);
+		}else if(data.error){
+			
+			if(data.error.isLoginRequired){
+				showLoginModal();
+			}			
 		}
 	};
+	//private methods
+	//Creat list item will create a single li it with users name and online status
 	function createListItem(item){
 
 		var link = document.createElement('a');
@@ -77,8 +96,11 @@
 			} else if (link.attachEvent)  {
 					link.attachEvent('onclick', function(){startChat(this)});
 			}
+		var span = document.createElement('span');
+			span.className = item.online_status;
 		var li = document.createElement('li');
 			li.className = "users";
+			li.appendChild(span);
 			li.appendChild(link);
 			
 		return li;
@@ -94,16 +116,18 @@
 			chatWindow = $('#chat_template').clone().attr('id',userId).removeClass("hide").prependTo('#chat_area');
 			chatWindow.find("#chatName").text(name);
 			chatWindow.find(".chat_header").toggle(function() {
-							  $(this).parentsUntil('.chatbox').parent().animate({
+							  $(this).parent(".chatbox:first").animate({
 													height: "30"
 												}, 500, function() {
 													// Animation complete.
+													console.log(this);
 												});
 							}, function() {
-							   $(this).parentsUntil('.chatbox').parent().animate({
+							   $(this).parent(".chatbox:first").animate({
 													height: "220"
 												}, 500, function() {
 													// Animation complete.
+													console.log(this);
 												});
 							}).find('.closeButton').click(function(event){
 							
